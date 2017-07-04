5b7988815625f8f1f32b87abaabcffc76446ae5e
Rawad Aboudlal <Rawadaboudlal@yahoo.com>
2017-07-04 15:20:06 -0400

    Implemented loop finding algorithm.
    - Creates an undirected graph of molecules hydrogen bonded together then
    finds loops using a depth first search algorithm.

--- loop.py
+++ loop.py
@@ -6,6 +6,53 @@ Created on Jun 30, 2017
 
 class Graph:
 	
-	def __init__(self, hbondList):
-		self.hbondList = hbondList
+	def __init__(self):
+		self.graph = {}
+	
+	def addGroup(self, a, b):
+		
+		if not a in self.graph:
+			self.graph[a] = []
+		
+		if not b in self.graph:
+			self.graph[b] = []
+		
+		if not a in self.graph[b]:
+			self.graph[b].append(a)
+		
+		if not b in self.graph[a]:
+			self.graph[a].append(b)
+	
+	def isCyclic(self):
+		
+		visited = {i: False for i in self.graph}
+		
+		for v in self.graph:
+			if not visited[v]:
+				if self.isCyclicUtil(visited, v, -1):
+					return True
+				
+		
+		return False
+	
+	def isCyclicUtil(self, visited, v, parent):
+		
+		visited[v] = True
+		
+		for u in self.graph[v]:
+			if not visited[u]:
+				if self.isCyclicUtil(visited, u, v):
+					return True
+			elif parent != u:
+				return True
+				
+		return False
 
+def createGraphFromHBonds(hbondList):
+	
+	graph = Graph()
+	
+	for hbond in hbondList:
+		graph.addGroup(hbond.mol1, hbond.mol2)
+	
+	return graph
